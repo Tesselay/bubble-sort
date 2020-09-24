5997f47477561bdc40a7526c7877cb9ce6cd48d6
lahl <domee.lahl@gmail.com>
2020-09-24 16:57:15 +0200

    Add test for negative numbers

--- test_sorting.py
+++ test_sorting.py
@@ -10,6 +10,13 @@ class TestSorting(unittest.TestCase):
         sorted_numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
         self.assertEqual(numbers, sorted_numbers, "Should be of same order")
 
+    def test_negative_list(self):
+        numbers = [-3, 2, -1, -7, 4, 9, -12, -11, 5, 7]
+        numbers = bubbleSort.bubble_sort(numbers)
+
+        sorted_numbers = [-12, -11, -7, -3, -1, 2, 4, 5, 7, 9]
+        self.assertEqual(numbers, sorted_numbers, "Should be of same order")
+
 
 if __name__ == '__main__':
     unittest.main()
