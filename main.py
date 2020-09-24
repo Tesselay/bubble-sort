94d1f75536df000231e7a3c57649a0939253bcc0
lahl <domee.lahl@gmail.com>
2020-09-24 16:27:34 +0200

    Replace main with functions

--- main.py
+++ main.py
@@ -8,25 +8,27 @@ def find_smallest_index(numbers):
     return numbers.index(smallest_val)
 
 
-if __name__ == '__main__':
-    list = [2,5,1,3,7,9,12,15,21,13,4, 16, 92, -2, 3]
+def iteration(numbers, iterator):
+    try:
+        if numbers[iterator] > numbers[iterator + 1]:
+            first_val = numbers[iterator]
+            second_val = numbers[iterator + 1]
+            numbers[iterator] = second_val
+            numbers[iterator + 1] = first_val
+        else:
+            iterator += 1
+    except IndexError:
+        iterator = 0
 
-    iterator = 0
 
-    smallest_index = find_smallest_index(list)
-    takeout = list.pop(smallest_index)
-    list.append(takeout)
+def bubble_sort(numbers):
+    iterator: int = 0
+
+    smallest_index = find_smallest_index(numbers)
+    takeout = numbers.pop(smallest_index)
+    numbers.append(takeout)
 
     while find_smallest_index(list) != 0:
-        try:
-            if list[iterator] > list[iterator+1]:
-                first_val = list[iterator]
-                second_val = list[iterator+1]
-                list[iterator] = second_val
-                list[iterator+1] = first_val
-            else:
-                iterator += 1
-        except IndexError:
-            iterator = 0
+        iteration(numbers, iterator)
 
-    print(list)
+    return numbers
