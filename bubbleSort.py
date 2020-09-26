46a1db7feee7167d19eb8b2491a3ffabb3f7d651
lahl <domee.lahl@gmail.com>
2020-09-26 20:37:01 +0200

    Segregate number append to own method

--- bubbleSort.py
+++ bubbleSort.py
@@ -18,12 +18,17 @@ def iteration(numbers, iterator):
     return numbers, iterator
 
 
+def move_to_end(numbers, number):
+    takeout = numbers.pop(number)
+    numbers.append(takeout)
+
+    return numbers
+
+
 def bubble_sort(numbers):
     iterator: int = 0
 
-    smallest_index = find_smallest_index(numbers)
-    takeout = numbers.pop(smallest_index)
-    numbers.append(takeout)
+    numbers = move_to_end(numbers, find_smallest_index(numbers))
 
     while find_smallest_index(numbers) != 0:
         numbers, iterator = iteration(numbers, iterator)
